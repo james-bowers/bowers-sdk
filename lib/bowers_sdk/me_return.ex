10baf9544b5c46b7bd38e180811869ad123bfc2b
James Bowers <james@james-bowers.me>
2019-04-09 16:50:13 +0100

    standardise fake data ids

--- lib/bowers_sdk/me_return.ex
+++ lib/bowers_sdk/me_return.ex
@@ -3,6 +3,16 @@ defmodule BowersSdk.MeReturn do
   Build valid returns from Me services
   for use in "business" logic tests.
   """
+  @fake_data [
+    person_id: "c6d771c9-debe-4276-bd32-d2ca2b2c394f",
+    account_id: "d75a9cd2-acec-46be-81e7-84a786971d44",
+    role_id: "f8c8a516-ad0a-4409-aca1-40c74b48d81a",
+    person_id: "c6d771c9-debe-4276-bd32-d2ca2b2c394f"
+  ]
+
+  def fake_data(id) when is_atom(id) do
+    Keyword.fetch!(@fake_data, id)
+  end
 
   def build_id, do: Ecto.UUID.generate()
 
@@ -23,18 +33,18 @@ defmodule BowersSdk.MeReturn do
       "description" => "Role validated",
       "content" => %{
         "role" => %{
-          "person_id" => "c6d771c9-debe-4276-bd32-d2ca2b2c394f",
-          "id" => "f8c8a516-ad0a-4409-aca1-40c74b48d81a",
-          "account_id" => "d75a9cd2-acec-46be-81e7-84a786971d44"
+          "person_id" => fake_data(:person_id),
+          "id" => fake_data(:role_id),
+          "account_id" => fake_data(:account_id)
         },
         "person" => %{
           "last_name" => "Bowers",
-          "id" => "c6d771c9-debe-4276-bd32-d2ca2b2c394f",
+          "id" => fake_data(:person_id),
           "first_name" => "James"
         },
         "email" => ["james@bowers.dev"],
         "account" => %{
-          "id" => "d75a9cd2-acec-46be-81e7-84a786971d44",
+          "id" => fake_data(:account_id),
           "active" => 1
         }
       }
@@ -50,9 +60,9 @@ defmodule BowersSdk.MeReturn do
       "content" => %{
         "roles" => [
           %{
-            "account_id" => "d75a9cd2-acec-46be-81e7-84a786971d44",
-            "id" => "f8c8a516-ad0a-4409-aca1-40c74b48d81a",
-            "person_id" => "c6d771c9-debe-4276-bd32-d2ca2b2c394f"
+            "account_id" => fake_data(:account_id),
+            "id" => fake_data(:role_id),
+            "person_id" => fake_data(:person_id)
           }
         ]
       },
