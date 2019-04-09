558bb0c638a06974e62f4ba2d98ab905a0ab1baf
James Bowers <james@james-bowers.me>
2019-04-09 16:41:14 +0100

    add mock reply data for a successful role validation response

--- lib/bowers_sdk/me_return.ex
+++ lib/bowers_sdk/me_return.ex
@@ -18,7 +18,27 @@ defmodule BowersSdk.MeReturn do
 
   def reply_to(:create_person, _opts), do: %{}
 
-  def reply_to(:validate_role, _opts), do: %{}
+  def reply_to(:validate_role, _opts),
+    do: %{
+      "description" => "Role validated",
+      "content" => %{
+        "role" => %{
+          "person_id" => "c6d771c9-debe-4276-bd32-d2ca2b2c394f",
+          "id" => "f8c8a516-ad0a-4409-aca1-40c74b48d81a",
+          "account_id" => "d75a9cd2-acec-46be-81e7-84a786971d44"
+        },
+        "person" => %{
+          "last_name" => "Bowers",
+          "id" => "c6d771c9-debe-4276-bd32-d2ca2b2c394f",
+          "first_name" => "James"
+        },
+        "email" => ["james@bowers.dev"],
+        "account" => %{
+          "id" => "d75a9cd2-acec-46be-81e7-84a786971d44",
+          "active" => 1
+        }
+      }
+    }
 
   def reply_to(:create_role),
     do: %{
