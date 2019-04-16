79e67d2c60604034a647d8622dcc31341ac8feff
James Bowers <james@james-bowers.me>
2019-04-16 06:56:04 +0100

    request with a bang, and provide content-type when POSTing, and parse response correctly

--- lib/bowers_sdk/http_client.ex
+++ lib/bowers_sdk/http_client.ex
@@ -4,16 +4,23 @@ defmodule BowersSdk.HTTPClient do
   @callback post(String.t(), String.t(), map()) :: response
 
   def get(host, path) do
-    HTTPoison.get("#{host}#{path}")
+    HTTPoison.get!("#{host}#{path}")
     |> format_response()
   end
 
   def post(host, path, payload) do
-    HTTPoison.post("#{host}#{path}", Jason.encode(payload))
+    HTTPoison.post!("#{host}#{path}", Jason.encode!(payload), [{"Content-Type", "application/json"}])
     |> format_response()
   end
 
   defp format_response(%HTTPoison.Response{status_code: status, body: body}) do
-    {status, Jason.decode!(body)}
+    {status, format_body(body)}
+  end
+
+  defp format_body(body) do
+    case Jason.decode(body) do
+      {:ok, json_response} -> json_response
+      {:error, _} -> body
+    end
   end
 end
